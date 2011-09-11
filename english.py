5b0d62f8aeb8d5055b7243c995d88d55e2d014df
Jonathan McCall <jonnymcc@gmail.com>
2011-09-11 00:18:13 -0400

    Cleaning some code, work on some language logic

--- english.py
+++ english.py
@@ -5,12 +5,29 @@ def pronoun_handler(msg):
 	second_per = ["you"]
 	third_per = ["he","she","they"]
 	
-	words = msg.split()
+	words = msg
 	if first_per in words:
 		msg = msg.replace()
 	
 	
-	
-	
-	
 	return
+
+# A start to handling pronouns... may be harder than I anticipate.
+def possesion_handler(msg):
+	words = msg
+
+	if ("my" or "mine") in words:
+		try: msg.remove("my"); msg.append("your"); 
+		except: pass
+		try: msg.remove("mine"); msg.append("yours"); 
+		except: pass
+		print "path 1"
+
+	elif ("your" or "yours") in words:
+		print "path 2"
+		try: msg.remove("your"); msg.append("my"); 
+		except: pass
+		try: msg.remove("yours"); msg.append("mine"); 
+		except: pass	 
+	
+	return msg
--- startbot.py
+++ startbot.py
@@ -2,65 +2,86 @@ import os
 import time
 import random
 import english
+import itertools
 from collections import defaultdict
 
 markov = defaultdict(list)
 STOP_WORD = "\n"
 
 class bot():
-	def __init__(self, chain_length = 1, chattiness = 1.0, max_words = 10000):
+	def __init__(self, chain_length = 2, max_words = 10000):
 		self.chain_length = chain_length
-		self.chattiness = chattiness
 		self.max_words = max_words
 	
 	def remember(self, msg):
 		add_to_brain(msg, self.chain_length, write_to_file=True)
 	
 	def reply(self, msg):
-		s = generate_sentences(msg, self.chain_length, self.max_words)
-		print "Nagisa: " + s.strip()
+		s = generate_reply(msg, self.chain_length, self.max_words)
+		# print "Nagisa: " + s.strip()
+		print "Nagisa: %s" % s
 
 def add_to_brain(msg, chain_length, write_to_file=False):
 	if write_to_file:
 		f = open('training_text.txt', 'a')
 		f.write(msg + '\n')
 		f.close()
-	buf = [STOP_WORD] * chain_length
+		
+	buf = []
+
+	print msg
 	for word in msg.split():
-		markov[tuple(buf)].append(word)
-		del buf[0]
+		print tuple(buf)
+		markov[tuple(buf)].append(msg)
+		# buf[0].pop()
 		buf.append(word)
-	markov[tuple(buf)].append(STOP_WORD)	
+		# print buf
+		# print markov
 
-def generate_sentences(msg, chain_length, max_words=10000):
+
+
+def generate_reply(msg, chain_length, max_words=10000):
 	buf = msg.split()[:chain_length]
-	if len(msg.split()) > chain_length:
-		message = buf[:]
-	else:
-		message = []
-		for i in xrange(chain_length):
-			message.append(random.choice(markov[random.choice(markov.keys())]))
-	for i in xrange(max_words):
-		try: 
-			next_word = random.choice(markov[tuple(buf)])
-		except IndexError:
-			continue
-		if next_word == STOP_WORD:
-			break
-		message.append(next_word)
-		del buf[0]
-		buf.append(next_word)
-	return ' '.join(message)
-	
 
+	if len(msg.split()) > chain_length:
+		response = buf[:]e
+	else:
+		response = []
+		for i in xrange(chain_length):
+			response.append(random.choice(markov[random.choice(markov.keys())]))
+	
+	buf = english.possesion_handler(buf)
+	
+	for x in itertools.permutations(buf):
+		try:
+			response = random.choice(markov[tuple(x)])
+		except:
+			pass
+	
+	# Random is boring...
+	# for i in xrange(max_words):
+		# try: 
+			# response = random.choice(markov[tuple(buf)])
+		# except IndexError:
+			# continue
+		# if next_word == STOP_WORD:
+			# break
+		# response.append(next_word)
+		# del buf[0]
+		# buf.append(next_word)
+	# return ' '.join(response)
+	return response
+	
+	
+# Starts program, not very interesting...
 if __name__ == "__main__":
 	if os.path.exists('training_text.txt'):
 		f = open('training_text.txt', 'r')
 		for line in f:
-			add_to_brain(line, chain_length=1)
-		print 'Brain Reloaded'
+			add_to_brain(line, chain_length=3)
 		f.close()
 	
+	print markov
 	nagisa = bot()
 	
 	user_msg = "start"
@@ -68,9 +89,8 @@ if __name__ == "__main__":
 	while user_msg.lower().split()[0] != "goodbye":
 		user_msg = raw_input(">")
 		
-		nagisa.remember(user_msg)
-		
 		if user_msg != "goodbye":
+			# nagisa.remember(user_msg)
 			nagisa.reply(user_msg)
 		else:
 			print "Nagisa: Goodbye!"
