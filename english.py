976b8e131ca6d093ac573db84b2bc7da41024c75
Jonathan McCall <jonnymcc@gmail.com>
2011-09-10 21:14:55 -0400

    Uploading starting files

---
+++ english.py
@@ -0,0 +1,16 @@
+import re
+
+def pronoun_handler(msg):
+	first_per = ["i"]
+	second_per = ["you"]
+	third_per = ["he","she","they"]
+	
+	words = msg.split()
+	if first_per in words:
+		msg = msg.replace()
+	
+	
+	
+	
+	
+	return
